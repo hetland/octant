f0c6a27619a9770cbcd1fd2d37f4de89096c91c2
hetland <hetland@443bacc9-c644-0410-9ac0-e35418122bc4>
2009-09-23 21:24:34 +0000

    added uvp_masks function to octant.grid
    git-svn-id: http://octant.googlecode.com/svn/trunk@52 443bacc9-c644-0410-9ac0-e35418122bc4

--- octant/grid.py
+++ octant/grid.py
@@ -1025,6 +1025,33 @@ class edit_mask_mesh(object):
         plt.draw()
     
 
+def uvp_masks(rmask):
+    '''
+    return u-, v-, and psi-masks based on input rho-mask
+    
+    Parameters
+    ----------
+    
+    rmask : ndarray
+        mask at CGrid rho-points
+    
+    Returns
+    -------
+    (umask, vmask, pmask) : ndarrays
+        masks at u-, v-, and psi-points
+    
+    '''
+    rmask = np.asarray(rmask)
+    assert rmask.ndim == 2, 'rmask must be a 2D array'
+    assert np.all((rmask==0)|(rmask==1)), 'rmask array must contain only ones and zeros.'
+    
+    umask = rmask[:, :-1] * rmask[:, 1:]
+    vmask = rmask[:-1, :] * rmask[1:, :]
+    pmask = rmask[:-1, :-1] * rmask[:-1, 1:] * rmask[1:, :-1] * rmask[1:, 1:]
+    
+    return umask, vmask, pmask
+
+
 if __name__ == '__main__':
     geographic = False
     if geographic:
