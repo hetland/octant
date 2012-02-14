521bc24b2246c7bf46031bdbfad3b372f465731c
hetland@gmail.com <hetland@gmail.com@443bacc9-c644-0410-9ac0-e35418122bc4>
2012-02-14 11:25:36 +0000

    Added pl33 class.
    Removed deprecated amin import from matplotlib.
    
    
    
    git-svn-id: http://octant.googlecode.com/svn/trunk@71 443bacc9-c644-0410-9ac0-e35418122bc4

--- octant/grid.py
+++ octant/grid.py
@@ -14,7 +14,6 @@ import matplotlib.pyplot as plt
 from matplotlib.artist import Artist
 from matplotlib.patches import Polygon, CirclePolygon
 from matplotlib.lines import Line2D
-from matplotlib.numerix.mlab import amin
 from matplotlib.mlab import dist_point_to_segment
 from matplotlib.nxutils import points_inside_poly
 from mpl_toolkits.basemap import pyproj
--- octant/tools.py
+++ octant/tools.py
@@ -94,6 +94,49 @@ def shrink(a,b):
     return a
 
 
+class plfilt(object):
+    """docstring for plfilt"""
+    
+    _pl33 = np.array([-0.00027, -0.00114, -0.00211, -0.00317, -0.00427, -0.00537,
+                      -0.00641, -0.00735, -0.00811, -0.00864, -0.00887, -0.00872,
+                      -0.00816, -0.00714, -0.0056 , -0.00355, -0.00097,  0.00213,
+                       0.00574,  0.0098 ,  0.01425,  0.01902,  0.024  ,  0.02911,
+                       0.03423,  0.03923,  0.04399,  0.04842,  0.05237,  0.05576,
+                       0.0585 ,  0.06051,  0.06174,  0.06215,  0.06174,  0.06051,
+                       0.0585 ,  0.05576,  0.05237,  0.04842,  0.04399,  0.03923,
+                       0.03423,  0.02911,  0.024  ,  0.01902,  0.01425,  0.0098 ,
+                       0.00574,  0.00213, -0.00097, -0.00355, -0.0056 , -0.00714,
+                      -0.00816, -0.00872, -0.00887, -0.00864, -0.00811, -0.00735,
+                      -0.00641, -0.00537, -0.00427, -0.00317, -0.00211, -0.00114,
+                      -0.00027], dtype='d')
+    
+    _dt = np.linspace(-33, 33, 67)
+    
+    def __init__(self, dt=1.0):
+        
+        if np.isscalar(dt):
+            self.dt = float(dt)
+        else:
+            self.dt = np.diff(dt).mean()
+        
+        filter_time = np.arange(0.0, 33.0, self.dt, dtype='d')
+        self.Nt = len(filter_time)
+        self.filter_time = np.hstack((-filter_time[-1:0:-1], filter_time))
+        
+        self.pl33 = np.interp(self.filter_time, self._dt, self._pl33)
+        self.pl33 /= self.pl33.sum()
+    
+    def __call__(self, u, t=None, mode='valid'):
+        uf = np.convolve(u, self.pl33, mode=mode)
+        if t is None:
+            return uf
+        else:
+            tf = t[self.Nt-1:-self.Nt+1]
+            return uf, tf
+
+
+
+
 def isoslice(z, q, zo=0, mode='spline'):
     """Return a slice a 3D field along an isosurface.
     
